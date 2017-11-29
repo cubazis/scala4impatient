0da3ea886bd8bd219de1ca049fab542b29bfa939
cubazis <grebenkin@phystech.edu>
2017-11-29 13:19:26 +0300

    Chapter3 first commit and ChapterN template

--- src/main/scala/Chapter3.scala
+++ src/main/scala/Chapter3.scala
@@ -3,6 +3,15 @@
   */
 object Chapter3 {
         def main(args: Array[String]): Unit = {
-
+                println("Here are some practice code from the text of Chapter 3")
+                repl()
+                println("Here are tasks code from the end of Chapter 3")
+                tasks()
         }
+
+        /** Here are some practice code from the text of Chapter 3 */
+        def repl(unit: Unit): Unit = {}
+
+        /** Here are tasks code from the end of Chapter 3 */
+        def tasks(unit: Unit): Unit = {}
 }
---
+++ src/main/scala/ChapterN.scala
@@ -0,0 +1,17 @@
+/**
+  * Created by cubazis on 29.11.17.
+  */
+object ChapterN {
+        def main(args: Array[String]): Unit = {
+                println("Here are some practice code from the text of Chapter N")
+                repl()
+                println("Here are tasks code from the end of Chapter N")
+                tasks()
+        }
+
+        /** Here are some practice code from the text of Chapter N */
+        def repl(unit: Unit): Unit = {}
+
+        /** Here are tasks code from the end of Chapter N */
+        def tasks(unit: Unit): Unit = {}
+}
